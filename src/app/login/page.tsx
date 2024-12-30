051ad052fd96e14a6ed4e4d6175b74f83e5a51a4
Sahil Udar <udarsahil0@gmail.com>
2024-12-30 17:55:22 +0530

    almost done final level

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 import axios from "axios";
 import toast from "react-hot-toast";
 
-export default function SignupPage() {
+export default function LoginPage() {
   const router = useRouter();
   const [user, setUser] = React.useState({
     username: "",
