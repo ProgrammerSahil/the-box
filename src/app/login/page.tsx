eb5af399cec946a25ac00be9eb3fc176265a6335
Sahil Udar <udarsahil0@gmail.com>
2025-01-03 22:53:54 +0530

    updating eslint

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -92,7 +92,7 @@ export default function LoginPage() {
             </form>
           </div>
           <div className="px-8 py-4 bg-gray-700 text-center">
-            <span className="text-gray-400">Don't have an account? </span>
+            <span className="text-gray-400">Don't have an account?</span>
             <a className="font-medium text-indigo-500 hover:text-indigo-400 cursor-pointer" onClick={onSignupPush}>
               Sign up
             </a>
